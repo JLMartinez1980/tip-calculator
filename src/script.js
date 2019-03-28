e30f2a36a5317f9e683d5bf85211dc70bba8cbb8
James Martinez <jamesmartinez@Jamess-Air.attlocal.net>
2019-03-28 14:24:38 -0500

    Total Bill to render

--- src/script.js
+++ src/script.js
@@ -1,1 +1,11 @@
-alert (hello)
+// const originalBill = () => {
+//   const cost = document.getElementById("cost").innerHTML;
+//   document.getElementById("renderedBill").innerHTML = cost;
+// }
+
+document.getElementById("cost").onchange = function() {renderedBill)};
+
+function renderedBill() {
+  const cost = document.getElementById("cost");
+  cost.value = cost.value.innerHTML("renderedBill")
+}
