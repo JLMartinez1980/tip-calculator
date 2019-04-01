398b5af2f33ede58da2dc471f7dc3563418b628b
James Martinez <jamesmartinez@Jamess-Air.attlocal.net>
2019-04-01 09:00:38 -0500

    almost there, not getting the % to render

--- src/script.js
+++ src/script.js
@@ -9,18 +9,6 @@ const renderBill = () => {
 const button = document.getElementById('saveAmount');
 button.addEventListener('click', renderBill);
 
-//Here is where I will figure out the experience/
-//pair that experience with a tip amount
-//make sure customer is happy with tip amount
-
-// I need to figure out how to return  innerhtml, i think to render the
-//experience and then attach that experience to a value..
-document.addEventListener('DOMContentLoaded', function() {
-  document.querySelector('select[name="experience"]').onchange=changeEventHandler;
-}, false);
-
-
-
 //use a switch to figure out the tip% attached to an experience
 const tipCal = (changeEventHandler) => {
   let percentage;
@@ -40,22 +28,19 @@ const tipCal = (changeEventHandler) => {
 
 console.log('1', tipCal);
 
+const myExperience = document.getElementById("form");
 
-//Thought 1  With this I do get somethign to render to the screen
-//just not the tip %
-const changeEventHandler = (event, tipCal) => {
-  if(!event.target.value) alert('please selct an experience');
+myExperience.addEventListener("change", tipCal);
+
+
+
+
+// Thought 1  With this I do get somethign to render to the screen
+// just not the tip %
+const changeEventHandler = (percentage, tipCal) => {
+  if(!event.target.value) return ('please selct an experience');
  else (document.getElementById("percentage").innerHTML = percentage)
- // alert('It looks like you had ' + event.target.value + ' experience');
+ alert('It looks like you had ' + event.target.value + ' experience');
  console.log('2', event);
  console.log('3', tipCal);
 }
-
-
-console.log('4', changeEventHandler());
-
-//Thought 2
-//here is where I want to use inner HTML to return a value asking if they
-//find the tip% acceptable
-document.getElementById("experience").addEventListener("change", tipCal).innerHTML
-= tipCal.value
