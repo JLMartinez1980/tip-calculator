34090b86c7152bdd7c2b8e148bd34bc9d34a65a4
James Martinez <jamesmartinez@Jamess-Air.attlocal.net>
2019-04-01 23:37:39 -0500

    got the tip% to render

--- src/script.js
+++ src/script.js
@@ -1,46 +1,35 @@
 //Need to use Rejex so numbers only can be used
-//This is for the bill before the tip, I will use This
-//to help figure out the total bill with the tip
-const renderBill = () => {
-  let bill = document.getElementById('reneredBill').value;
-  document.getElementById('enteredBill').innerHTML = bill;
+//This is for the bill before the tip.
+function changeEventHandler({ value }) {
+  const tipPercentage = tipCal(value);
+  const bill = document.getElementById('reneredBill').value;
+  const total = tipPercentage * Number(bill);
+  document.getElementById("percentage").innerHTML = `Total: $${total}`;
 }
 
-const button = document.getElementById('saveAmount');
-button.addEventListener('click', renderBill);
-
 //use a switch to figure out the tip% attached to an experience
-const tipCal = (changeEventHandler) => {
-  let percentage;
-  switch(changeEventHandler) {
-    case "EXCELLENT":
-      return tip = 11/50;
-    case "GREAT":
-     return tip = 1/5;
+//don't put eventhandler, just pass it a value
+//make sure to match the value on the html side,
+function tipCal(value) {
+  switch(value) {
+    case "excellent":
+      return 11/50;
+    case "great":
+     return 1/5;
       break;
-    case "GOOD":
-      return tip = 9/50;
+    case "good":
+      return 9/50;
       break;
-    case "EH":
-     return tip = 1/10;
+    case "eh":
+     return 1/10;
   }
 }
 
-console.log('1', tipCal);
+const button = document.getElementById('leaveTip');
+button.addEventListener('click', calculateBill);
 
-const myExperience = document.getElementById("form");
-
-myExperience.addEventListener("change", tipCal);
-
-
-
-
-// Thought 1  With this I do get somethign to render to the screen
-// just not the tip %
-const changeEventHandler = (tipCal, percentage) => {
-  if(!event.target.value) return ('please selct an experience');
- else (document.getElementById("percentage").innerHTML = tipCal)
- alert('It looks like you had ' + event.target.value + ' experience');
- console.log('2', event);
- console.log('3', tipCal);
+function calculateBill(bill, total) {
+  console.log("bill", bill);
+  console.log('total', total);
+  return total + bill;
 }
