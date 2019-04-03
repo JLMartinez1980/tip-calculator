3893e04ece0c523e74f4622766559ff6c63f6d4d
James Martinez <jamesmartinez@Jamess-Air.attlocal.net>
2019-04-03 08:31:26 -0500

    getting the yes button to work

--- src/script.js
+++ src/script.js
@@ -1,15 +1,8 @@
-//Need to use Rejex so numbers only can be used
-//This is for the bill before the tip.
-function changeEventHandler({ value }) {
-  const tipPercentage = tipCal(value);
-  const bill = document.getElementById('reneredBill').value;
-  const total = tipPercentage * Number(bill);
-  document.getElementById("percentage").innerHTML = `Total: $${total}`;
-}
 
 //use a switch to figure out the tip% attached to an experience
-//don't put eventhandler, just pass it a value
+//don't put eventhandler as an argument, just pass it a value
 //make sure to match the value on the html side,
+//do not use return "value" = 1/10
 function tipCal(value) {
   switch(value) {
     case "excellent":
@@ -25,11 +18,29 @@ function tipCal(value) {
   }
 }
 
+//Need to use Rejex so numbers only can be used
+//This is for the bill before the tip.
+function changeEventHandler({ value }) {
+  const tipPercentage = tipCal(value);
+  const bill = document.getElementById('reneredBill').value;
+  const total = tipPercentage * Number(bill);
+  document.getElementById("percentage").innerHTML = `Total: $${total}`;
+}
+
+function calculateBill({ value }) {
+  console.log(value, 'longShot')
+  const newTotal = document.getElementById('reneredBill').value;
+  console.log(newTotal, 'thisWorks');
+  const newTip = parseFloat(document.getElementById("percentage").value);
+  console.log(newTip, "thisDoesnt");
+  console.log("percentage", percentage);
+  const newTipPercentage = tipCal(value);
+  console.log('notYet', newTipPercentage);
+  console.log('uhuh', value);
+
+  // document.getElementById("showMeTheMoney").innerHTML = `$${newTotal + newTip}`;
+}
+
+
 const button = document.getElementById('leaveTip');
 button.addEventListener('click', calculateBill);
-
-function calculateBill(bill, total) {
-  console.log("bill", bill);
-  console.log('total', total);
-  return total + bill;
-}
